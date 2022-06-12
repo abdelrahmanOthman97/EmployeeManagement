de39770eecbde890ff5cfcf8fa1893a880401696
Abdelrahman <abdalrahmanosmandokhan@yahoo.com>
2022-06-13 00:48:49 +0200

    set table responsive

--- EmployeeManagement.WebUI/wwwroot/js/site.js
+++ EmployeeManagement.WebUI/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//----when submit form , disabled this submit button
+$('input[type="submit"]').click(function () {
+    let btn = $(this);
+    $(btn).prop('disabled', true);
+    let form = $(btn).parents().find('form');
+    $(form).submit();
+    setTimeout(() => {
+        $(btn).prop('disabled', false);
+    }, 3000)
+});
